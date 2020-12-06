42a406885d6342bfd915398c06791e417ca51cff
Yong Bok Ko <kyb01179@gmail.com>
2020-12-06 17:19:38 +0900

    테스트 데이터 생성

---
+++ server/prisma/seed.ts
@@ -0,0 +1,43 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.post.deleteMany({});
+  await prisma.user.deleteMany({});
+  for (let i = 1; i <= 100; i++) {
+    await prisma.user.create({ data: createUser(i) });
+  }
+}
+
+function createUser(i: number) {
+  let pad = ('' + i).padStart(3, '0');
+  let user = {
+    email: `user${pad}@test.com`,
+    name: '사용자' + pad,
+    posts: {
+      create: createPosts(pad),
+    },
+  };
+  return user;
+}
+
+function createPosts(pad: string) {
+  let posts = [];
+  let pad2 = '';
+  for (let k = 1; k <= 10; k++) {
+    pad2 = ('' + k).padStart(3, '0');
+    posts.push({
+      title: `제목${pad}-${pad2}`,
+      content: `내용${pad}-${pad2}`,
+      published: k % 2 === 0,
+    });
+  }
+  return posts;
+}
+
+main()
+  .catch((e) => console.error(e))
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
